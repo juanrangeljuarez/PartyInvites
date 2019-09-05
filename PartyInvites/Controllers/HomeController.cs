edcfed9590e43f513d5900ae6f8eebc01269b482
juanrangeljuarez <juanrangel1@mail.weber.edu>
2019-09-04 23:22:26 -0600

    new changes

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using PartyInvites.Models;
-
+using System.Linq;
 namespace PartyInvites.Controllers
 {
     public class HomeController : Controller
@@ -15,22 +11,29 @@ namespace PartyInvites.Controllers
             int hour = DateTime.Now.Hour;
             ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
             return View("MyView");
-        }  
-        
+        }
         [HttpGet]
         public ViewResult RsvpForm()
         {
-
             return View();
         }
-
         [HttpPost]
         public ViewResult RsvpForm(GuestResponse guestResponse)
         {
-            //To do: Store Guest Response
-
-            //Step 2 chapter 14 on the book
-            return View();
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                // there is a validation error
+                return View();
+            }
+        }
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
         }
     }
 }
